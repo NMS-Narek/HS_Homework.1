d34716be3bfe75a303ede641e5d371aae130c787
User <User@WIN-0NJAL0A78BC>
2021-08-10 20:44:06 +0400

    Add project files.

---
+++ HS_Homework.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HS_Homework._1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+        static void Test()
+        {
+            Console.WriteLine("The entry point was successfully changed !!!");
+            Console.ReadKey();
+        }
+    }
+}
